bdd4c307cde324d53addc4ab34eb5ca27e276e7a
gladson-sza <gladsonsouza@live.com>
2021-02-12 12:00:10 -0400

    Added display, refactor components code and folders

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import './style.css';
+
+const Button = props => <button className="button">{props.label}</button>
+export default Button
---
+++ src/components/Display/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import './style.css'
+
+const Display = props => <div className="display">{props.value}</div>
+export default Display
